1e62b3471eabb3dbb3431e88ebd676fcaa513741
zokerop <99017614+zokerop@users.noreply.github.com>
2024-04-05 20:56:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+
+
+const navbarToggle = document.querySelector('.navbar-toggle');
+const navbarMenu = document.querySelector('.navbar-menu');
+
+navbarToggle.addEventListener('click', () => {
+  navbarToggle.classList.toggle('active');
+  navbarMenu.classList.toggle('active');
+});
